860085100bf857fa117fd0b39155a81acd0812ea
deepspacediver <luka.koscik@gmail.com>
2022-11-03 22:16:49 +0100

    class constructors with intial append function adding if list is empty

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello')
+class Node {
+  constructor(value, next){
+    this.value= value;
+    this.next=null;
+  }
+}
+
+
+class LinkedList {
+  constructor(){
+    this.list={}
+  }
+  append(value) {
+    if(Object.entries(this.list).length === 0) this.list = new Node(value)
+  }
+}
+
+let myLinkedList = new LinkedList()
