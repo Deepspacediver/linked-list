8b50a96e5b8523c1c2dedb49e06490095e21a4d5
deepspacediver <luka.koscik@gmail.com>
2022-11-04 14:28:44 +0100

    prepend and getSize methods, seperated method to check if a list is empty

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class Node {
-  constructor(value, next) {
+  constructor(value, next = null) {
     this.value = value;
-    this.next = null;
+    this.next = next;
   }
 }
 
@@ -9,13 +9,33 @@ class LinkedList {
   constructor() {
     this.list = {};
   }
+  isListEmpty() {
+    return Object.entries(this.list).length === 0;
+  }
   append(value) {
-    if (Object.entries(this.list).length === 0) this.list = new Node(value);
+    if (this.isListEmpty()) this.list = new Node(value);
     else {
       let nextNode = this.list;
       while (nextNode.next !== null) nextNode = nextNode.next;
       nextNode.next = new Node(value);
     }
+  }
+  prepend(value) {
+    if (this.isListEmpty()) this.list = new Node(value);
+    else {
+      const previousList = this.list;
+      this.list = new Node(value, previousList);
+    }
+  }
+  getSize() {
+    if (this.isListEmpty()) return 0;
+    let total = 1;
+    let nextNode = this.list;
+    while (nextNode.next !== null) {
+      total += 1;
+      nextNode = nextNode.next;
+    }
+    return total;
   }
 }
 
