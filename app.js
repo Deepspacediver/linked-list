e0a21d581660cabf9d304476410b1d4e7ca72975
deepspacediver <luka.koscik@gmail.com>
2022-11-04 18:53:38 +0100

    replaced list with head in LinkedList class, refactored methods to work with the List as an initially empty object

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ class Node {
 }
 
 class LinkedList {
-  constructor() {
-    this.list = {};
+  constructor(head = null) {
+    this.head = head;
   }
   isListEmpty() {
-    return Object.entries(this.list).length === 0;
+    return this.head === null;
   }
   append(value) {
-    if (this.isListEmpty()) this.list = new Node(value);
+    if (this.isListEmpty()) this.head = new Node(value);
     else {
-      let nextNode = this.list;
+      let nextNode = this.head;
       while (nextNode.next !== null) nextNode = nextNode.next;
       nextNode.next = new Node(value);
     }
@@ -23,20 +23,24 @@ class LinkedList {
   prepend(value) {
     if (this.isListEmpty()) this.list = new Node(value);
     else {
-      const previousList = this.list;
-      this.list = new Node(value, previousList);
+      const previousList = this.head;
+      this.head = new Node(value, previousList);
     }
   }
   getSize() {
     if (this.isListEmpty()) return 0;
     let total = 1;
-    let nextNode = this.list;
+    let nextNode = this.head;
     while (nextNode.next !== null) {
       total += 1;
       nextNode = nextNode.next;
     }
     return total;
   }
+  getHead() {
+    if (this.isListEmpty()) return "Empty list";
+    else return this.head;
+  }
 }
 
-let myLinkedList = new LinkedList();
+let myLinkedList = new LinkedList(new Node("base"));
